70a165a3a00a1d543c559b6cb1ca3df0f1b92486
Andrey Markov <andrewshasucks@gmail.com>
2021-03-12 19:01:49 +1000

    ....................

--- AssemblerLab6/AssemblerLab6.cpp
+++ AssemblerLab6/AssemblerLab6.cpp
@@ -43,7 +43,7 @@ int main() {
 	generateQuadraticMatrices(m1, m1SSE);
 	generateQuadraticMatrices(m2, m2SSE);
 
-	vector[0] = 3;
+	vector[0] = 1;
 	for (int i = 1; i < N; ++i) {
 		vector[i] = vector[0];
 	}
@@ -54,75 +54,52 @@ int main() {
 	start = __rdtsc();
 	_asm {
 	INITIALISATION:
-		mov ecx, N
-		// пусть ebx будет номером k-го элемента матрицы
-		xor ebx, ebx
+		mov    ecx, N
+		xor    ebx, ebx                                   // пусть ebx будет номером k-го элемента матрицы
 
 	DECREASE_M1:
-		// копируем в xmm0 i-ю строку матрицы m1SSE
-		movups xmm0, [m1SSE + 4 * ebx]
-		// копируем в xmm1 вектор, состоящий из N заданных чисел k
-		movups xmm1, [vector]
-		// уменьшаем в k раз i-ю строку матрицы
-		divps xmm0, xmm1
-		// вставляем обратно строку в матрицу, но уже уменьшенную в k раз
-		movups [m1SSE + 4 * ebx], xmm0
+		movups xmm0, [m1SSE + 4 * ebx]                    // копируем в xmm0 i-ю строку матрицы m1SSE
+		movups xmm1, [vector]                             // копируем в xmm1 вектор, состоящий из N заданных чисел k
+		divps  xmm0, xmm1                                 // уменьшаем в k раз i-ю строку матрицы
+		movups [m1SSE + 4 * ebx], xmm0                    // вставляем обратно строку в матрицу, но уже уменьшенную в k раз
 		
 	ROW_COMPOSITING:
-		// пусть edx будет номером j-го столбца
-		xor edx, edx
+		finit                                             // инициализация сопроцессора
+		xor edx, edx                                      // пусть edx будет номером j-го столбца
+	
 	ELEMENT_COMPOSITING:
 		// сохраняем в column j-й столбец матрицы m2SSE
-		fld[m2SSE + 4 * edx + 48]
-		fld[m2SSE + 4 * edx + 32]
-		fld[m2SSE + 4 * edx + 16]
-		fld[m2SSE + 4 * edx]
-		fstp[column]
-		fstp[column + 4]
-		fstp[column + 8]
-		fstp[column + 12]
-		// создаем копию i-й строки в xmm1
-		movups xmm1, xmm0
-		// перемножаем i-ю строку с j-м столбцом поэлементно
-		mulps xmm1, [column]
-		// переносим полученный вектор в column
-		movups [column], xmm1
-		// сохраняем сумму всех элементов полученного вектора
-		fld[column]
-		fld[column + 4]
-		fadd
-		fld[column + 8]
-		fadd
-		fld[column + 12]
-		fadd
-		fstp[eax]
-		// сохраняем сумму eax в k-ый элемент матрицы-произведение
-		mov compositionSSE[ebx], eax
-		// увеличиваем ebx и edx на единицу
-		inc ebx
-		inc edx
+		fld    [m2SSE + 4 * edx + 48]
+		fld    [m2SSE + 4 * edx + 32]
+		fld    [m2SSE + 4 * edx + 16]
+		fld    [m2SSE + 4 * edx]
+		fstp   [column]
+		fstp   [column + 4]
+		fstp   [column + 8]
+		fstp   [column + 12]
+
+		movups xmm1, xmm0                                 // создаем копию i-й строки в xmm1
+		mulps  xmm1, [column]                             // перемножаем i-ю строку с j-м столбцом поэлементно
+		movups [column], xmm1                             // переносим полученный вектор в column
+		
+		// сохраняем сумму всех элементов вектора
+		fld    [column]
+		fld    [column + 4]
+		fld    [column + 8]
+		fld    [column + 12]
+		faddp  st(1), st(0)
+		faddp  st(1), st(0)
+		faddp  st(1), st(0)
+		
+		fstp   [compositionSSE + 4 * ebx]                 // сохраняем сумму в k-ый элемент матрицы-произведение
 
 		// повторяем всё это для каждого элемента строки
-		cmp edx, N
-		jne ELEMENT_COMPOSITING
-		
+		inc    ebx
+		inc    edx
+		cmp    edx, N
+		jne    ELEMENT_COMPOSITING
 
-	/*COMPOSITION_OF_M1_AND_M2:	
-		mov ebx, 0
-	
-	GETTING_M2_COLUMN:
-		mov edx, 0
-		lea */
-
-
-		/*mulps xmm1, xmm0
-		movups[m2SSE + 4 * eax], xmm1
-
-		addps xmm0, xmm1
-		movups[res1 + 4 * eax], xmm0
-
-		add eax, 4
-		loop PROCESS*/
+		loop   DECREASE_M1
 	}
 	end = __rdtsc();
 	std::cout << m1SSE << std::endl;
