a527394a178be2abb9324cf641dc1abad452be98
markov-avl <andrewshasucks@gmail.com>
2021-03-13 02:40:29 +1000

    lab6 SSE test completed

--- AssemblerLab6/AssemblerLab6.cpp
+++ AssemblerLab6/AssemblerLab6.cpp
@@ -2,16 +2,21 @@
 #include <intrin.h>
 #include <random>
 #include <ctime>
-
 #define N 4
-#define FP 3400000000
 #define MAX_RANDOM 20
+
+/*
+* ВАРИАНТ 7
+* 
+* Размерность матриц 4х4. Уменьшить все элементы матрицы М1 в заданное число раз. Умножить М1 на матрицу М2.
+* Сложить поэлементно М1 и матрицу-произведение.
+*/
 
 typedef float QuadraticMatrix[N][N];
 
 
 std::ostream& operator<<(std::ostream& out, const QuadraticMatrix& matrix) {
-	out.precision(3);
+	out.precision(7);
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < N; ++j) {
 			out << matrix[i][j] << '\t';
@@ -43,13 +48,21 @@ int main() {
 	generateQuadraticMatrices(m1, m1SSE);
 	generateQuadraticMatrices(m2, m2SSE);
 
-	vector[0] = 1;
+	// НЕ ЗАБЫТЬ ДОБАВИТЬ ВВОД ПОЛЬЗОВАТЕЛЯ
+	vector[0] = 2;
 	for (int i = 1; i < N; ++i) {
 		vector[i] = vector[0];
 	}
 	
-	std::cout << "M1 matrix:" << std::endl << m1 << std::endl;
-	std::cout << "M2 matrix:" << std::endl << m2 << std::endl;
+	std::cout << "M1:" << std::endl << m1 << std::endl;
+	std::cout << "M2:" << std::endl << m2 << std::endl;
+
+	/*
+	В результате SSE ассемблерной вставки будет:
+	- в m1SSE будет храниться M1 / m;
+	- в compositionSSE будет храниться (M1 / m) * M2.
+	- в m2SSE будет храниться M1 + (M1 / m) * M2;
+	*/
 
 	start = __rdtsc();
 	_asm {
@@ -58,16 +71,16 @@ int main() {
 		mov    ecx, N
 		xor    ebx, ebx                                   // пусть ebx будет номером k-го элемента матрицы
 
-	DECREASE_M1:
+	DECREASE:
 		movups xmm0, [m1SSE + 4 * ebx]                    // копируем в xmm0 i-ю строку матрицы m1SSE
-		movups xmm1, [vector]                             // копируем в xmm1 вектор, состоящий из N заданных чисел k
-		divps  xmm0, xmm1                                 // уменьшаем в k раз i-ю строку матрицы
-		movups [m1SSE + 4 * ebx], xmm0                    // вставляем обратно строку в матрицу, но уже уменьшенную в k раз
+		movups xmm1, [vector]                             // копируем в xmm1 вектор, состоящий из N заданных чисел m
+		divps  xmm0, xmm1                                 // уменьшаем в m раз i-ю строку матрицы
+		movups [m1SSE + 4 * ebx], xmm0                    // вставляем обратно строку в матрицу, но уже уменьшенную в m раз
 		
-	ROW_COMPOSITING:
-		xor edx, edx                                      // пусть edx будет номером j-го столбца
+	ROW_COMPOSITION:
+		xor    edx, edx                                   // пусть edx будет номером j-го столбца
 	
-	ELEMENT_COMPOSITING:
+	ELEMENT_COMPOSITION:
 		// сохраняем в column j-й столбец матрицы m2SSE
 		fld    [m2SSE + 4 * edx + 48]
 		fld    [m2SSE + 4 * edx + 32]
@@ -79,7 +92,8 @@ int main() {
 		fstp   [column + 12]
 
 		movups xmm1, xmm0                                 // создаем копию i-й строки в xmm1
-		mulps  xmm1, [column]                             // перемножаем i-ю строку с j-м столбцом поэлементно
+		movups xmm2, [column]                             // копируем в xmm2 j-й столбец
+		mulps  xmm1, xmm2                                 // перемножаем i-ю строку с j-м столбцом поэлементно
 		movups [column], xmm1                             // переносим полученный вектор в column
 		
 		// сохраняем сумму всех элементов вектора
@@ -96,17 +110,35 @@ int main() {
 		inc    ebx
 		inc    edx
 		cmp    edx, N
-		jne    ELEMENT_COMPOSITING
+		jne    ELEMENT_COMPOSITION
 
+		// повторяем всё это для каждой строки
 		dec    ecx
-		cmp    ecx, N
-		jne    DECREASE_M1
+		cmp    ecx, 0
+		jne    DECREASE
+
+	ADDITION:
+		xor    ebx, ebx                                   // пусть ebx будет номером i-х строк матриц
+
+	ROW_ADDITION:
+		movups xmm0, [m1SSE + 4 * ebx]                    // сохраняем i-ю строку матрицы M1 в xmm0
+		movups xmm1, [compositionSSE + 4 * ebx]           // сохраняем i-ю строку матрицы (M1 / m) * M2 в xmm1
+		addps  xmm1, xmm0                                 // суммируем строки и сохраняем результат в xmm1
+		movups [m2SSE + 4 * ebx], xmm1                    // сохраняем xmm1 в i-ю строку матрицы M2
+
+		// повторяем всё это для каждой строки
+		add    ebx, 4
+		cmp    ebx, N * N
+		jne    ROW_ADDITION
 	}
 	end = __rdtsc();
 	
-	std::cout << end - start << " ticks" << std::endl;
-	std::cout << m1SSE << std::endl;
-	std::cout << compositionSSE << std::endl;
+	// результаты теста SSE
+	std::cout << std::endl << "SSE TEST RESULTS:" << std::endl << std::endl;
+	std::cout << "M1 / " << vector[0] << ":" << std::endl << m1SSE << std::endl;
+	std::cout << "M1 / " << vector[0] << " * M2:" << std::endl << compositionSSE << std::endl;
+	std::cout << "M1 + (M1 / " << vector[0] << ") * M2:" << std::endl << m2SSE << std::endl;
+	std::cout << "Time spent: " << end - start << " ticks" << std::endl;
 
 	return 0;
 }
