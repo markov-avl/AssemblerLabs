56670c8309c2a99baae2624f280e110e24ea5555
markov-avl <andrewshasucks@gmail.com>
2021-05-21 03:08:46 +1000

    7.1 completed

---
+++ AssemblerLab7.2/AssemblerLab7.2.cpp
@@ -0,0 +1,119 @@
+﻿#include <windows.h>
+#include <iostream>
+
+
+extern "C" void masm();
+
+extern "C" void* stdHandle(
+	DWORD handler
+) {
+	return GetStdHandle(
+		handler
+	);
+}
+
+extern "C" BOOL readConsole(
+	HANDLE                       hConsoleInput,
+	LPVOID                       lpBuffer,
+	DWORD                        nNumberOfCharsToRead,
+	LPDWORD                      lpNumberOfCharsRead,
+	PCONSOLE_READCONSOLE_CONTROL pInputControl
+) {
+	return ReadConsole(
+		hConsoleInput,
+		lpBuffer,
+		nNumberOfCharsToRead,
+		lpNumberOfCharsRead,
+		pInputControl
+	);
+}
+
+extern "C" HANDLE createFile(
+	LPCSTR                lpFileName,
+	DWORD                 dwDesiredAccess,
+	DWORD                 dwShareMode,
+	LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+	DWORD                 dwCreationDisposition,
+	DWORD                 dwFlagsAndAttributes,
+	HANDLE                hTemplateFile
+) {
+	return CreateFileA(
+		lpFileName,
+		dwDesiredAccess,
+		dwShareMode,
+		lpSecurityAttributes,
+		dwCreationDisposition,
+		dwFlagsAndAttributes,
+		hTemplateFile
+	);
+}
+
+extern "C" BOOL writeFile(
+	HANDLE       hFile,
+	LPCVOID      lpBuffer,
+	DWORD        nNumberOfBytesToWrite,
+	LPDWORD      lpNumberOfBytesWritten,
+	LPOVERLAPPED lpOverlapped
+) {
+	return WriteFile(
+		hFile,
+		lpBuffer,
+		nNumberOfBytesToWrite * 2,
+		lpNumberOfBytesWritten,
+		lpOverlapped
+	);
+}
+
+extern "C" BOOL closeHandle(
+	HANDLE hObject
+) {
+	return CloseHandle(
+		hObject
+	);
+}
+
+extern "C" VOID exitProcess(
+	UINT resp
+) {
+	return ExitProcess(
+		resp
+	);
+}
+
+extern "C" BOOL writeConsole(
+	HANDLE      hConsoleOutput,
+	CONST VOID* lpBuffer,
+	DWORD       nNumberOfCharsToWrite,
+	LPDWORD     lpNumberOfCharsWritten,
+	LPVOID      lpReserved
+) {
+	return WriteConsole(
+		hConsoleOutput,
+		lpBuffer,
+		nNumberOfCharsToWrite * 2,
+		lpNumberOfCharsWritten,
+		lpReserved
+	);
+}
+
+extern "C" BOOL readFile(
+	HANDLE       hFile,
+	LPVOID       lpBuffer,
+	DWORD        nNumberOfBytesToRead,
+	LPDWORD      lpNumberOfBytesRead,
+	LPOVERLAPPED lpOverlapped
+) {
+	return ReadFile(
+		hFile,
+		lpBuffer,
+		nNumberOfBytesToRead,
+		lpNumberOfBytesRead,
+		lpOverlapped
+	);
+}
+
+
+int main() {
+	masm();
+	return 0;
+}
--- AssemblerLab7.2/src.asm
+++ AssemblerLab7.2/src.asm
@@ -4,8 +4,9 @@ PUBLIC masm
 .model flat, C
 option casemap : none
 
-BUFFER_SIZE      = 128
-STD_INPUT_HANDLE = -10
+BUFFER_SIZE        = 128
+STD_INPUT_HANDLE   = -10
+STD_OUTPUT_HANDLER = -11
 
 .data
 
@@ -13,18 +14,23 @@ outputHandler dd 0
 inputHandler  dd 0
 fileHandler   dd 0
 
+nWritten      dd 0
 nRead         dd 0
+bytesCount    dd 0
 bytesWritten  dd 0
 
-consoleFile   db BUFFER_SIZE DUP(?)
+fileBuffer    db BUFFER_SIZE DUP(?)
+consoleBuffer db BUFFER_SIZE DUP(?)
 filename      db "output.txt", 0
 
 .code
 
 EXTERN stdHandle:PROC
+EXTERN writeConsole:PROC
 EXTERN readConsole:PROC
 EXTERN createFile:PROC
 EXTERN writeFile:PROC
+EXTERN readFile:PROC
 EXTERN closeHandle:PROC
 EXTERN exitProcess:PROC
 
@@ -32,6 +38,38 @@ masm PROC
 
 push ebp
 mov  ebp, esp
+
+; меняем дескриптор на вывод
+push STD_OUTPUT_HANDLER
+call stdHandle
+mov  outputHandler, eax
+
+; создаем файл output.txt, если он не создан
+push 0
+push 80h
+push 3
+push 0
+push 2h
+push 3
+push offset filename
+call createFile
+mov  fileHandler, eax
+
+; читаем из файла
+push 0
+push offset bytesCount
+push BUFFER_SIZE
+push offset fileBuffer
+push fileHandler
+call readFile
+
+; вывод файла в консоль
+push 0
+push offset nWritten
+push bytesCount
+push offset fileBuffer
+push outputHandler
+call writeConsole
 
 ; меняем дескриптор на считывание
 push STD_INPUT_HANDLE
@@ -42,26 +80,15 @@ mov  inputHandler, eax
 push 0
 push offset nRead
 push BUFFER_SIZE
-push offset consoleFile
+push offset consoleBuffer
 push inputHandler
 call readConsole
-
-; создаем файл output.txt, если он не создан
-push 0
-push 80h
-push 2
-push 0
-push 2h
-push 2
-push offset filename
-call createFile
-mov  fileHandler, eax
 
 ; записываем в файл введеную строку
 push 0
 push offset bytesWritten
 push nRead
-push offset consoleFile
+push offset consoleBuffer
 push fileHandler
 call writeFile
 
